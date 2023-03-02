6d817e378ed49db380b157acdd5a3c7a4ec34f1d
ronaldo castro <rcastro@alasxpress.com>
2023-03-02 17:15:15 -0300

    primera subida hello world

---
+++ app/routes/index.jsx
@@ -0,0 +1,15 @@
+export const meta = () => {
+    return {
+      title: 'Hydrogen',
+      description: 'A custom storefront powered by Hydrogen',
+    };
+  };
+  
+  export default function Index() {
+    return (
+      <div>
+        <h3>Hello from the home page ronaldo!</h3>
+      </div>
+    );
+  }
+  
